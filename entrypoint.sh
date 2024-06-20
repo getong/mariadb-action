a912bfddaaf8fe28b82c7ba0fccf3dbb6e306f0a
Daniel Black <daniel@mariadb.org>
2024-06-20 11:14:20 +1000

    use a bash array for command arguments
    Readd healtcheck commands that correspond to docker-run man page

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-docker_run="docker run"
+declare -a docker_run=( docker run )
 
 if [ -n "$INPUT_MYSQL_ROOT_PASSWORD" ]; then
   echo "Root password not empty, use root superuser"
 
-  docker_run="$docker_run -e MYSQL_ROOT_PASSWORD=$INPUT_MYSQL_ROOT_PASSWORD"
+  docker_run+=( -e MYSQL_ROOT_PASSWORD="$INPUT_MYSQL_ROOT_PASSWORD" )
 elif [ -n "$INPUT_MYSQL_USER" ]; then
   if [ -z "$INPUT_MYSQL_PASSWORD" ]; then
     echo "The mysql password must not be empty when mysql user exists"
@@ -14,24 +14,24 @@ elif [ -n "$INPUT_MYSQL_USER" ]; then
 
   echo "Use specified user and password"
 
-  docker_run="$docker_run -e MYSQL_RANDOM_ROOT_PASSWORD=true -e MYSQL_USER=$INPUT_MYSQL_USER -e MYSQL_PASSWORD=$INPUT_MYSQL_PASSWORD"
+  docker_run+=( -e MYSQL_RANDOM_ROOT_PASSWORD=true -e MYSQL_USER="$INPUT_MYSQL_USER" -e MYSQL_PASSWORD="$INPUT_MYSQL_PASSWORD" )
 else
   echo "Using empty password for root"
 
-  docker_run="$docker_run -e MYSQL_ALLOW_EMPTY_PASSWORD=true"
+  docker_run+=(-e MYSQL_ALLOW_EMPTY_PASSWORD=true )
 fi
 
 if [ -n "$INPUT_MYSQL_DATABASE" ]; then
   echo "Use specified database"
 
-  docker_run="$docker_run -e MYSQL_DATABASE=$INPUT_MYSQL_DATABASE"
+  docker_run+=( -e MYSQL_DATABASE="$INPUT_MYSQL_DATABASE" )
 fi
 
-docker_run="$docker_run --health-cmd='healthcheck.sh --connect --innodb_initialized'"
-docker_run="$docker_run -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT $INPUT_MARIADB_IMAGE:$INPUT_MARIADB_VERSION --port=$INPUT_CONTAINER_PORT"
-docker_run="$docker_run --character-set-server=$INPUT_CHARACTER_SET_SERVER --collation-server=$INPUT_COLLATION_SERVER"
+docker_run+=( --health-cmd='healthcheck.sh --connect --innodb_initialized' --health-start-interval=20s --health-start-interval=3s )
+docker_run+=( -d -p "$INPUT_HOST_PORT:$INPUT_CONTAINER_PORT" "$INPUT_MARIADB_IMAGE:$INPUT_MARIADB_VERSION" --port="$INPUT_CONTAINER_PORT" )
+docker_run+=( --character-set-server="$INPUT_CHARACTER_SET_SERVER" --collation-server="$INPUT_COLLATION_SERVER" )
 
-CONTAINER_NAME=$(eval "$docker_run" )
+CONTAINER_NAME=$( "${docker_run[@]}" )
 
 echo "Waiting for container $CONTAINER_NAME to start..."
 
